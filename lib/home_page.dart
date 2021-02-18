ef8573bf332c63ea83eab9c5fd97e95654006293
khanhlh21197 <lek21197@gmail.com>
2021-02-18 13:26:14 +0700

    clean code home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -591,223 +591,238 @@ class _MapViewState extends State<MapView> {
         body: Stack(
           children: <Widget>[
             // Map View
-            GoogleMap(
-              markers: markers != null ? Set<Marker>.from(markers) : null,
-              initialCameraPosition: _initialLocation,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: false,
-              mapType: MapType.normal,
-              zoomGesturesEnabled: true,
-              zoomControlsEnabled: false,
-              polylines: Set<Polyline>.of(polylines.values),
-              onMapCreated: (GoogleMapController controller) {
-                mapController = controller;
-              },
-            ),
+            mapContainer(),
             // Show zoom buttons
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    ClipOval(
-                      child: Material(
-                        color: Colors.blue[100], // button color
-                        child: InkWell(
-                          splashColor: Colors.blue, // inkwell color
-                          child: SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: Icon(Icons.add),
-                          ),
-                          onTap: () {
-                            mapController.animateCamera(
-                              CameraUpdate.zoomIn(),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ClipOval(
-                      child: Material(
-                        color: Colors.blue[100], // button color
-                        child: InkWell(
-                          splashColor: Colors.blue, // inkwell color
-                          child: SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: Icon(Icons.remove),
-                          ),
-                          onTap: () {
-                            mapController.animateCamera(
-                              CameraUpdate.zoomOut(),
-                            );
-                          },
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            zoomButtons(),
             // Show the place input fields & button for
             // showing the route
-            SafeArea(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                    ),
-                    width: width * 0.9,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            'Places',
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                          SizedBox(height: 10),
-                          _textField(
-                              label: 'Vị trí của bạn',
-                              hint: 'Chọn điểm bắt đầu',
-                              prefixIcon: Icon(Icons.looks_one),
-                              suffixIcon: IconButton(
-                                icon: Icon(Icons.my_location),
-                                onPressed: () {
-                                  startAddressController.text = _currentAddress;
-                                  _startAddress = _currentAddress;
-                                },
-                              ),
-                              controller: startAddressController,
-                              width: width,
-                              locationCallback: (String value) {
-                                setState(() {
-                                  _startAddress = value;
-                                });
-                              }),
-                          SizedBox(height: 10),
-                          _textField(
-                              label: 'Vị trí xe bus',
-                              hint: 'Chọn điểm đến',
-                              prefixIcon: Icon(Icons.looks_two),
-                              controller: destinationAddressController,
-                              width: width,
-                              locationCallback: (String value) {
-                                setState(() {
-                                  _destinationAddress = value;
-                                });
-                              }),
-                          SizedBox(height: 10),
-                          Visibility(
-                            visible: _placeDistance == null ? false : true,
-                            child: Text(
-                              'Khoảng cách: $_placeDistance km',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          RaisedButton(
-                            onPressed: (_startAddress != '' &&
-                                    _destinationAddress != '')
-                                ? () async {
-                                    setState(() {
-                                      if (markers.isNotEmpty) markers.clear();
-                                      if (polylines.isNotEmpty)
-                                        polylines.clear();
-                                      if (polylineCoordinates.isNotEmpty)
-                                        polylineCoordinates.clear();
-                                      _placeDistance = null;
-                                    });
+            directContainer(width),
+            // Show current location button
+            currentLocationButton(),
+          ],
+        ),
+      ),
+    );
+  }
 
-                                    _calculateDistance().then((isCalculated) {
-                                      if (isCalculated) {
-                                        _scaffoldKey.currentState.showSnackBar(
-                                          SnackBar(
-                                            content: Text(
-                                                'Tính khoảng cách thành công'),
-                                          ),
-                                        );
-                                      } else {
-                                        _scaffoldKey.currentState.showSnackBar(
-                                          SnackBar(
-                                            content: Text('Có lỗi xảy ra'),
-                                          ),
-                                        );
-                                      }
-                                    });
-                                  }
-                                : null,
-                            color: Colors.red,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                'Chỉ đường'.toUpperCase(),
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+  Widget mapContainer() {
+    return GoogleMap(
+      markers: markers != null ? Set<Marker>.from(markers) : null,
+      initialCameraPosition: _initialLocation,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      mapType: MapType.normal,
+      zoomGesturesEnabled: true,
+      zoomControlsEnabled: false,
+      polylines: Set<Polyline>.of(polylines.values),
+      onMapCreated: (GoogleMapController controller) {
+        mapController = controller;
+      },
+    );
+  }
+
+  Widget zoomButtons() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ClipOval(
+              child: Material(
+                color: Colors.blue[100], // button color
+                child: InkWell(
+                  splashColor: Colors.blue, // inkwell color
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Icon(Icons.add),
                   ),
+                  onTap: () {
+                    mapController.animateCamera(
+                      CameraUpdate.zoomIn(),
+                    );
+                  },
                 ),
               ),
             ),
-            // Show current location button
-            SafeArea(
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
-                  child: ClipOval(
-                    child: Material(
-                      color: Colors.orange[100], // button color
-                      child: InkWell(
-                        splashColor: Colors.orange, // inkwell color
-                        child: SizedBox(
-                          width: 56,
-                          height: 56,
-                          child: Icon(Icons.my_location),
-                        ),
-                        onTap: () {
-                          mapController.animateCamera(
-                            CameraUpdate.newCameraPosition(
-                              CameraPosition(
-                                target: LatLng(
-                                  _currentPosition.latitude,
-                                  _currentPosition.longitude,
-                                ),
-                                zoom: 18.0,
-                              ),
-                            ),
-                          );
+            SizedBox(height: 20),
+            ClipOval(
+              child: Material(
+                color: Colors.blue[100], // button color
+                child: InkWell(
+                  splashColor: Colors.blue, // inkwell color
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Icon(Icons.remove),
+                  ),
+                  onTap: () {
+                    mapController.animateCamera(
+                      CameraUpdate.zoomOut(),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget directContainer(double width) {
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+            ),
+            width: width * 0.9,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Places',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  SizedBox(height: 10),
+                  _textField(
+                      label: 'Vị trí của bạn',
+                      hint: 'Chọn điểm bắt đầu',
+                      prefixIcon: Icon(Icons.looks_one),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.my_location),
+                        onPressed: () {
+                          startAddressController.text = _currentAddress;
+                          _startAddress = _currentAddress;
                         },
                       ),
+                      controller: startAddressController,
+                      width: width,
+                      locationCallback: (String value) {
+                        setState(() {
+                          _startAddress = value;
+                        });
+                      }),
+                  SizedBox(height: 10),
+                  _textField(
+                      label: 'Vị trí xe bus',
+                      hint: 'Chọn điểm đến',
+                      prefixIcon: Icon(Icons.looks_two),
+                      controller: destinationAddressController,
+                      width: width,
+                      locationCallback: (String value) {
+                        setState(() {
+                          _destinationAddress = value;
+                        });
+                      }),
+                  SizedBox(height: 10),
+                  Visibility(
+                    visible: _placeDistance == null ? false : true,
+                    child: Text(
+                      'Khoảng cách: $_placeDistance km',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 5),
+                  RaisedButton(
+                    onPressed:
+                        (_startAddress != '' && _destinationAddress != '')
+                            ? () async {
+                                setState(() {
+                                  if (markers.isNotEmpty) markers.clear();
+                                  if (polylines.isNotEmpty) polylines.clear();
+                                  if (polylineCoordinates.isNotEmpty)
+                                    polylineCoordinates.clear();
+                                  _placeDistance = null;
+                                });
+
+                                _calculateDistance().then((isCalculated) {
+                                  if (isCalculated) {
+                                    _scaffoldKey.currentState.showSnackBar(
+                                      SnackBar(
+                                        content:
+                                            Text('Tính khoảng cách thành công'),
+                                      ),
+                                    );
+                                  } else {
+                                    _scaffoldKey.currentState.showSnackBar(
+                                      SnackBar(
+                                        content: Text('Có lỗi xảy ra'),
+                                      ),
+                                    );
+                                  }
+                                });
+                              }
+                            : null,
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Chỉ đường'.toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget currentLocationButton() {
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
+          child: ClipOval(
+            child: Material(
+              color: Colors.orange[100], // button color
+              child: InkWell(
+                splashColor: Colors.orange, // inkwell color
+                child: SizedBox(
+                  width: 56,
+                  height: 56,
+                  child: Icon(Icons.my_location),
+                ),
+                onTap: () {
+                  mapController.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: LatLng(
+                          _currentPosition.latitude,
+                          _currentPosition.longitude,
+                        ),
+                        zoom: 18.0,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ),
       ),
     );
